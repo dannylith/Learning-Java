edc6e7382536ee2e14fbd1affaa75aded67daea0
dlithyouvong <dlithyouvong@MCBA212-C22.classrooms.stlcc>
2020-01-28 20:11:41 -0600

    Add welcome project

---
+++ Welcome/src/welcome/Welcome.java
@@ -0,0 +1,19 @@
+/*Author: Danny Lithyouvong
+ *Date: 1/28/20
+ *Desc: display welcome message
+ *File: C:\Users\dlithyouvong\Documents\Learning-Java
+ */
+package welcome;
+
+
+public class Welcome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // display welcome message
+        System.out.println("Welcome to Java");
+    }
+    
+}
